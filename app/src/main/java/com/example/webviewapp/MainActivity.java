4cb50ddf4bb8ff95a1855362fc2941ee0436c4c3
b19karhj <b19karhj@student.his.se>
2021-04-06 12:09:12 +0200

    Fixed so I can call the function for internal and external.

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -21,12 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
+        WebView myWebView = findViewById(R.id.my_webview);
         myWebView.loadUrl("https://www.google.com/");
 
     }
 
     public void showInternalWebPage(){
         // TODO: Add your code for showing internal web page here
+        WebView myWebView = findViewById(R.id.my_webview);
         myWebView.loadUrl("File:///android_asset/First.html");
     }
 
@@ -99,16 +101,14 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
             Log.d("==>","Will display external web page");
-            WebView myWebView = findViewById(R.id.my_webview);
-            myWebView.loadUrl("https://www.google.com/");
+            showExternalWebPage();
             return true;
 
         }
 
         if (id == R.id.action_internal_web) {
             Log.d("==>","Will display internal web page");
-            WebView myWebView = findViewById(R.id.my_webview);
-            myWebView.loadUrl("File:///android_asset/First.html");
+            showInternalWebPage();
             return true;
 
         }
